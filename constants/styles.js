7a01a4bf5e46fb0cd2cb5be4219cbe05d3a90693
Lorion Moreira de Jesus <lorion.jesus@dpe.ba.gov.br>
2024-04-18 16:55:26 -0300

    feito na sala

---
+++ constants/styles.js
@@ -0,0 +1,15 @@
+export const Colors = {
+  primary100: '#EDEDED',
+  primary500: '#023047',
+  primary800: '#939393',
+  error100: '#fcdcbf',
+  error500: '#f37c13',
+}
+
+
+/*
+#8ecae6
+#219ebc
+#ffb703
+#fb8500
+*/
